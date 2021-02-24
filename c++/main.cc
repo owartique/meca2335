bd92f624f62e0a27f68a47f1cc0a3fdf6f5b9e89
Olivier <olivier.wartique@student.uclouvain.be>
2021-02-24 19:33:03 +0100

    cleaning

--- c++/main.cc
+++ c++/main.cc
@@ -3,12 +3,27 @@
 *	RHex - Gr4
 */
 
-#include "param.h"
+//#include "param.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 
 
+int main(){
 
-void main(){
+	ifstream myReadFile;
+	myReadFile.open("../log/output.txt");	
+	char output[100];
+	if(myReadFile.is_open()){
+		while(!myReadFile.eof()){
+			myReadFile >> output;
+			cout << output << cendl;
+		}
+	}
+
+	myReadFile.close();
 
 	return 0;
 
